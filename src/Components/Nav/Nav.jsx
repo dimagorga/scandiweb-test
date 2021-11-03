fa281d0ad2484ea1a2a06ea875f150b4140a318d
Dzmitry Harhun <dimagorga@gmail.com>
2021-11-03 20:01:15 +0300

    add goBack btn

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,4 +1,6 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, withRouter } from "react-router-dom";
+
+import { PureComponent } from "react";
 import { Query } from "react-apollo";
 import s from "./Nav.module.css";
 import logo from "../../Images/logo.jpg";
@@ -6,51 +8,62 @@ import NavCartButton from "../NavCartButton/NavCartButton";
 import NavCurrencyBtn from "../NavCurrencyBtn/NavCurrencyBtn";
 import { categoriesNameRequest } from "../../services/gql-requests";
 
-export default function Nav() {
-  return (
-    <Query query={categoriesNameRequest()}>
-      {({ loading, error, data }) => {
-        if (loading) return <p>Loading...</p>;
-        if (error) return <p>Error : </p>;
-        const { category, categories } = data;
-        return (
-          <div className={s.wrapper}>
-            <ul className={s.list}>
-              <li className={s.item}>
-                <NavLink
-                  to={`/${category.name}`}
-                  className={s.link}
-                  activeClassName={s.activeLink}
-                >
-                  {category.name.toUpperCase()}
-                </NavLink>
-              </li>
-              {categories.map((cat) => {
-                return (
-                  <li key={cat.name} className={s.item}>
-                    <NavLink
-                      to={`/${cat.name}`}
-                      className={s.link}
-                      activeClassName={s.activeLink}
-                    >
-                      {cat.name.toUpperCase()}
-                    </NavLink>
-                  </li>
-                );
-              })}
-            </ul>
-            <img className={s.logo} src={logo} alt="logo" />
-            <ul className={s.secondaryNav}>
-              <li>
-                <NavCurrencyBtn />
-              </li>
-              <li>
-                <NavCartButton />
-              </li>
-            </ul>
-          </div>
-        );
-      }}
-    </Query>
-  );
+class Nav extends PureComponent {
+  goBack = () => {
+    const { history, location } = this.props;
+    return history.push(location?.state?.from ?? "/all");
+  };
+  render() {
+    return (
+      <Query query={categoriesNameRequest()}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error : </p>;
+          const { category, categories } = data;
+          return (
+            <div className={s.wrapper}>
+              <ul className={s.list}>
+                <li className={s.item}>
+                  <NavLink
+                    to={`/${category.name}`}
+                    className={s.link}
+                    activeClassName={s.activeLink}
+                  >
+                    {category.name.toUpperCase()}
+                  </NavLink>
+                </li>
+                {categories.map((cat) => {
+                  return (
+                    <li key={cat.name} className={s.item}>
+                      <NavLink
+                        to={`/${cat.name}`}
+                        className={s.link}
+                        activeClassName={s.activeLink}
+                      >
+                        {cat.name.toUpperCase()}
+                      </NavLink>
+                    </li>
+                  );
+                })}
+              </ul>
+              <button className={s.goBackBtn} onClick={this.goBack}>
+                <img className={s.logo} src={logo} alt="logo" />
+              </button>
+
+              <ul className={s.secondaryNav}>
+                <li>
+                  <NavCurrencyBtn />
+                </li>
+                <li>
+                  <NavCartButton />
+                </li>
+              </ul>
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
 }
+
+export default withRouter(Nav);
